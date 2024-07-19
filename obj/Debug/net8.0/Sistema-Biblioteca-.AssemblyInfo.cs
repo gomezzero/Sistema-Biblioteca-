1fb948a3c4a1bbb5ce3b52d6e54f658a92840f28
gomezzero <gomejuanzero@gmail.com>
2024-07-19 08:55:29 -0500

    cd

--- obj/Debug/net8.0/Sistema-Biblioteca-.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema-Biblioteca-.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema-Biblioteca-")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45aa6b78703a402fcccef222dcb86d02919f5fbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f0f24d4f13bcc788d8eb0ac8c67245489e9973a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema-Biblioteca-")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema-Biblioteca-")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
